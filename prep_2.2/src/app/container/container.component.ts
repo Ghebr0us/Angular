dc84a8ce9f0a339466faaa983e64ddc26c82c27c
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2022-10-06 10:53:12 +0000

    punto 4

--- prep_2.2/src/app/container/container.component.ts
+++ prep_2.2/src/app/container/container.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-  name : string[]
+  m_name : string[]
   constructor() {
-    this.name = ['ruspa','escavatore','bulldozer','minipala','rulli compattatori','dumper','camion betoniere','gru','motolivellatrici','abbattitrici forestali']
+    this.m_name = ['ruspa','escavatore','bulldozer','minipala','rulli compattatori','dumper','camion betoniere','gru','motolivellatrici','abbattitrici forestali']
    }
 
   ngOnInit(): void {
