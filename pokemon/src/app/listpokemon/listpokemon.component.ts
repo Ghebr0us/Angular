e97cd50fad716f59835a529738704224f6d2916f
Ghebr0us <ghebrous.davide@iisgalvanimi.edu.it>
2023-11-09 12:58:10 +0000

    bono infame

--- pokemon/src/app/listpokemon/listpokemon.component.ts
+++ pokemon/src/app/listpokemon/listpokemon.component.ts
@@ -1,10 +1,25 @@
+import{HttpClient} from '@angular/common/http'
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import{ListaPokemon,Pokemon} from '../models/pokemon.model'
 @Component({
   selector: 'app-listpokemon',
   templateUrl: './listpokemon.component.html',
   styleUrls: ['./listpokemon.component.css']
 })
-export class ListpokemonComponent {
 
-}
+
+export class ListpokemonComponent {
+   id!: string;
+   List!:ListaPokemon;
+  constructor(public http:HttpClient, private route:ActivatedRoute) {
+    this.route.params.subscribe(paramsId => {
+      this.id = paramsId['path'];
+      console.log(this.id);
+  });
+  this.http.get<ListaPokemon>(`https://pokeapi.co/api/v2/type/${this.id}`)
+  
+  }
+    
+  
+ }
